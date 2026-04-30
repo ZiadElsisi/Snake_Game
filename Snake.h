90717d42559dccb691df99aa0928aa4dd4e6a6c3
3b3aty-collab <body90492@gmail.com>
2026-04-30 15:14:20 +0300

    Add Snake class

--- Snake.h
+++ Snake.h
@@ -1,4 +1,32 @@
-#pragma once
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <deque>
+#include <algorithm>
 #include "Shared.h"
 
-class Snake {};
+class Snake
+{
+public:
+    Snake();
+
+    void reset();
+
+    void move(Direction d); // push_front newHead, pop_back tail
+
+    void grow(Direction d); // push_front newHead, no pop_back
+
+    Cell getHead() const;
+
+    const Body &getBody() const;
+
+    bool containsCell(const Cell &c) const; // O(n) linear search
+
+    int length() const;
+
+private:
+    Body body_;
+    void computeNextHead(Direction d, Cell &out) const;
+};
+
+#endif
