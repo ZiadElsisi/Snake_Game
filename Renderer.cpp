c9a69afb985cbbb083d9ddc30c073ee0fe0630a0
Ziad <ziadsisi79@gmail.com>
2026-04-30 19:16:21 +0300

    Remove build files

---
+++ Renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ziad Elsisi on 30/04/2026.
+//
+
+#include "Renderer.h"
---
+++ Renderer.h
@@ -0,0 +1,13 @@
+//
+// Created by Ziad Elsisi on 30/04/2026.
+//
+
+#ifndef SNAKEGAME_RENDERER_H
+#define SNAKEGAME_RENDERER_H
+
+
+class Renderer {
+};
+
+
+#endif //SNAKEGAME_RENDERER_H
